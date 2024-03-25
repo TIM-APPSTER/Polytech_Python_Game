2d00875a96b5868e982149295a7b8ba21a33ee6f
S-1-5-21-2662224114-325094091-2635250640-1001 <mega.mitenkov@gmail.com>
2024-03-25 14:33:10 +0200

    Ver 2.0 Add animation

--- Game/Main.py
+++ Game/Main.py
@@ -1,6 +1,38 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 
+
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(enemy):
+        ball_speed_y *= -1
+
+
+def player_animation():
+    if player.left <= 0:
+        player.left = 0
+    if player.right >= screen_width:
+        player.right = screen_width
+
+
+def enemy_animation():
+    if enemy.left <= 0:
+        enemy.left = 0
+    if enemy.right >= screen_width:
+        enemy.right = screen_width
+    if enemy.left < ball.x:
+        enemy.left += ball_speed_x
+    if enemy.right > ball.x:
+        enemy.right -= ball_speed_x
+
+
 # pygame setup
 pygame.init()
 screen_width = 720
@@ -11,10 +43,15 @@ running = True
 dt = 0
 pygame.display.set_caption("Ping-Pong")
 
-player = pygame.Rect(screen_width / 2 - 15, screen_height / 2 + 300, 50, 5)
-ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 10, 10)
-enemy = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 300, 50, 5)
+player = pygame.Rect(screen_width / 2 - 25, screen_height - 25, 50, 5)
+ball = pygame.Rect(screen_width / 2 - 5, screen_height / 2 - 5, 10, 10)
+enemy = pygame.Rect(screen_width / 2 - 25, 25, 50, 5)
+bg_color = pygame.Color('grey12')
 
+ball_speed_x = 2
+ball_speed_y = 2
+player_speed = 0
+enemy_speed = 7
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -22,19 +59,22 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
-    pygame.draw.rect(screen, 'gray', player)
-
-    pygame.draw.ellipse(screen, 'gray', ball)
-    pygame.draw.rect(screen, 'gray', enemy)
-
-
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
         player.x -= 300 * dt
     if keys[pygame.K_d]:
         player.x += 300 * dt
+
+    ball_animation()
+    player_animation()
+    enemy_animation()
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, 'gray', player)
+    pygame.draw.ellipse(screen, 'gray', ball)
+    pygame.draw.rect(screen, 'gray', enemy)
+    pygame.draw.aaline(screen, 'gray', (screen_height, screen_height / 2), (0, screen_height / 2))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
