2e1824e895ef66c64ea8463390a484dcf4169c22
S-1-5-21-2662224114-325094091-2635250640-1001 <mega.mitenkov@gmail.com>
2024-03-25 12:38:37 +0200

    Ver 1.0 Add 2 rectangles and ball

--- Game/Main.py
+++ Game/Main.py
@@ -3,9 +3,17 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen_width = 720
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+pygame.display.set_caption("Ping-Pong")
+
+player = pygame.Rect(screen_width / 2 - 15, screen_height / 2 + 300, 50, 5)
+ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 10, 10)
+enemy = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 300, 50, 5)
 
 while running:
     # poll for events
@@ -15,13 +23,22 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
+    pygame.draw.rect(screen, 'gray', player)
 
-    # RENDER YOUR GAME HERE
+    pygame.draw.ellipse(screen, 'gray', ball)
+    pygame.draw.rect(screen, 'gray', enemy)
+
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        player.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player.x += 300 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    dt = clock.tick(120) / 700
 
 pygame.quit()
