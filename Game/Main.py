1dfe8680dc20a8fbebea1a52f905e06438611d8b
S-1-5-21-2662224114-325094091-2635250640-1001 <mega.mitenkov@gmail.com>
2024-03-26 14:39:13 +0200

    New Version 2.5 - Refactoring

--- Game/Main.py
+++ Game/Main.py
@@ -1,13 +1,35 @@
 import random
+
 import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 720
+SCREEN_HEIGHT = 720
+BALL_SPEED_X = 2
+BALL_SPEED_Y = 2
+
+player_score = 0
+enemy_score = 0
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+pygame.display.set_caption("Ping-Pong")
+
+player = pygame.Rect(SCREEN_WIDTH / 2 - 25, SCREEN_HEIGHT - 25, 50, 5)
+ball = pygame.Rect(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2 - 5, 10, 10)
+enemy = pygame.Rect(SCREEN_WIDTH / 2 - 25, 25, 50, 5)
+bg_color = pygame.Color('grey12')
 
 
 def ball_animation():
-    global ball_speed_x, ball_speed_y, player_score, enemy_score
+    global BALL_SPEED_X, BALL_SPEED_Y, player_score, enemy_score
 
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-    if ball.top <= 0 or ball.bottom >= screen_height:
+    ball.x += BALL_SPEED_X
+    ball.y += BALL_SPEED_Y
+    if ball.top <= 0 or ball.bottom >= SCREEN_HEIGHT:
         if ball.y <= 0.5:
             player_score += 1
             pygame.mixer.music.load("Materials/ping_pong_8bit_peeeeeep.ogg")
@@ -19,59 +41,39 @@ def ball_animation():
             pygame.mixer.music.play()
             ball_restart()
 
-    if ball.left <= 0 or ball.right >= screen_width:
+    if ball.left <= 0 or ball.right >= SCREEN_WIDTH:
         pygame.mixer.music.load("Materials/ping_pong_8bit_plop.ogg")
         pygame.mixer.music.play()
-        ball_speed_x *= -1
+        BALL_SPEED_X *= -1
 
     if ball.colliderect(player) or ball.colliderect(enemy):
         pygame.mixer.music.load("Materials/ping_pong_8bit_beeep.ogg")
         pygame.mixer.music.play()
-        ball_speed_y *= -1
+        BALL_SPEED_Y *= -1
 
 
 def ball_restart():
-    global ball_speed_y, ball_speed_x
-    ball.center = (screen_width / 2, screen_height / 2)
-    ball_speed_y *= random.choice((1, -1))
-    ball_speed_x *= random.choice((1, -1))
+    global BALL_SPEED_Y, BALL_SPEED_X
+    ball.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    BALL_SPEED_Y *= random.choice((1, -1))
+    BALL_SPEED_X *= random.choice((1, -1))
 
 
 def player_animation():
     if player.left <= 0:
         player.left = 0
-    if player.right >= screen_width:
-        player.right = screen_width
+    if player.right >= SCREEN_WIDTH:
+        player.right = SCREEN_WIDTH
 
 
 def enemy_animation():
     if enemy.left <= 0:
         enemy.left = 0
-    if enemy.right >= screen_width:
-        enemy.right = screen_width
+    if enemy.right >= SCREEN_WIDTH:
+        enemy.right = SCREEN_WIDTH
 
-
-pygame.init()
-screen_width = 720
-screen_height = 720
-screen = pygame.display.set_mode((screen_width, screen_height))
-clock = pygame.time.Clock()
-running = True
-dt = 0
-pygame.display.set_caption("Ping-Pong")
-
-player = pygame.Rect(screen_width / 2 - 25, screen_height - 25, 50, 5)
-ball = pygame.Rect(screen_width / 2 - 5, screen_height / 2 - 5, 10, 10)
-enemy = pygame.Rect(screen_width / 2 - 25, 25, 50, 5)
-bg_color = pygame.Color('grey12')
-
-ball_speed_x = 2
-ball_speed_y = 2
-player_score = 0
-enemy_score = 0
 
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -96,10 +98,10 @@ while running:
     pygame.draw.rect(screen, 'gray', player)
     pygame.draw.ellipse(screen, 'gray', ball)
     pygame.draw.rect(screen, 'gray', enemy)
-    pygame.draw.aaline(screen, 'gray', (screen_height, screen_height / 2), (0, screen_height / 2))
+    pygame.draw.aaline(screen, 'gray', (SCREEN_HEIGHT, SCREEN_HEIGHT / 2), (0, SCREEN_HEIGHT / 2))
     screen.blit(
         pygame.font.SysFont('arial', 30, bold=True).render(f'score: {player_score} - {enemy_score}', True, 'white'),
-        (screen_width / 2 - 50, screen_height / 2))
+        (SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2))
 
     pygame.display.flip()
 
