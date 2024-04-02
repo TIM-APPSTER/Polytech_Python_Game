b8d30301d362fecd13be64ff92264aa3ba2b3d3e
S-1-5-21-2662224114-325094091-2635250640-1001 <mega.mitenkov@gmail.com>
2024-04-02 16:57:15 +0300

    Ver 0.5 Alpha Add class Racket and Ball

--- Game/main.py
+++ Game/main.py
@@ -1,15 +1,22 @@
 import pygame
 
 import constants
-from game_objects import PlayerRackets
+from game_objects import Racket, Ball
+
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 screen.fill(constants.SCREEN_COLOR)
 
+first_player_coordinate = (constants.SCREEN_WIDTH / 2 - 25, constants.SCREEN_HEIGHT - 25)
+first_player_racket = Racket(screen, first_player_coordinate[0], first_player_coordinate[1], pygame.K_a,
+                             pygame.K_d)
 
-player_rackets = PlayerRackets(screen)
+second_player_coordinate = (constants.SCREEN_WIDTH / 2 - 25, 25)
+second_player_rackets = Racket(screen, second_player_coordinate[0], second_player_coordinate[1], pygame.K_LEFT,
+                               pygame.K_RIGHT)
 
+ball = Ball(screen)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -17,8 +24,14 @@ while True:
 
     screen.fill(constants.SCREEN_COLOR)
 
+    first_player_racket.draw()
+    first_player_racket.move()
 
-    player_rackets.draw()
-    player_rackets.move(5)
+    second_player_rackets.draw()
+    second_player_rackets.move()
+
+    ball.draw()
+    ball.move()
+
     pygame.display.flip()
     clock.tick(constants.FPS)
