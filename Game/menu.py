56c9a78db6afc5120e994ace233c3bd514e19f9f
Home_PC <freelance.timone@gmail.com>
2024-04-05 07:49:40 +0300

    Create line 2.0 and bad score

--- Game/menu.py
+++ Game/menu.py
@@ -1,3 +1,15 @@
 class Menu:
-    def __init__(self):
-        self.menu = []
+    def __init__(self, screen):
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+
+    def show_menu(self):
+        self.screen.fill = 'Black'
+        # self.play_button.show()
+        # self.exit_button.show()
+
+    def play_button(self):
+        pass
+
+    def exit_button(self):
+        pass
