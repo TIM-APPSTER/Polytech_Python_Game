054590ec9cbf8e4954dbf56283fc7a4e62f0bfc6
S-1-5-21-2662224114-325094091-2635250640-1001 <mega.mitenkov@gmail.com>
2024-04-23 10:19:56 +0300

    ver 2.0 Add worked countdown

--- Game/game_objects.py
+++ Game/game_objects.py
@@ -1,6 +1,3 @@
-import time
-import random
-
 import pygame
 
 import constants
@@ -51,7 +48,6 @@ class Ball(pygame.Rect):
         self.y_speed = constants.BALL_SPEED
         self.dt = 5
         self.screen = screen
-        self.score_time = True
 
     def moving(self):
         self.x += self.x_speed
@@ -63,29 +59,12 @@ class Ball(pygame.Rect):
             self.x_speed *= -1
 
     def restart(self):
+        self.x = constants.SCREEN_WIDTH / 2
+        self.y = constants.SCREEN_HEIGHT / 2
         self.countdown()
 
     def countdown(self):
-        basic_font = pygame.font.Font('Materials/Roboto-Light.ttf', 32)
-        self.center = (constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
-        current_time = pygame.time.get_ticks()
-
-        if current_time - self.score_time < 700:
-            number_three = basic_font.render("3", False, 'white')
-            self.screen.blit(number_three, (constants.SCREEN_WIDTH / 2 - 10,  constants.SCREEN_HEIGHT / 2 + 20))
-        if 700 < current_time - self.score_time < 1400:
-            number_two = basic_font.render("2", False, 'white')
-            self.screen.blit(number_two, (constants.SCREEN_WIDTH / 2 - 10,  constants.SCREEN_HEIGHT / 2 + 20))
-        if 1400 < current_time - self.score_time < 2100:
-            number_one = basic_font.render("1", False, 'white')
-            self.screen.blit(number_one, (constants.SCREEN_WIDTH / 2 - 10,  constants.SCREEN_HEIGHT / 2 + 20))
-
-        if current_time - self.score_time < 2100:
-            self.y_speed, self.x_speed = 0, 0
-        else:
-            self.x_speed = 7 * random.choice((1, -1))
-            self.y_speed = 7 * random.choice((1, -1))
-            self.score_time = None
+        pass
 
     def draw(self):
         pygame.draw.ellipse(self.screen, 'gray', self)
--- Game/main.py
+++ Game/main.py
@@ -24,6 +24,30 @@ is_pause = False
 
 menu = Menu(screen)
 
+count_flag = False
+basic_font = pygame.font.SysFont('Materials/Roboto-Light.ttf', 200)
+num = 3
+
+
+def print_countdown(score_time):
+    current_time = pygame.time.get_ticks()
+    time_elapsed = current_time - score_time
+
+    if time_elapsed < 700:
+        number = 3
+    elif time_elapsed < 1400:
+        number = 2
+    elif time_elapsed < 2100:
+        number = 1
+    else:
+        return  # Countdown finished
+
+    countdown_text = basic_font.render(str(number), False, 'white', 'black')
+    text_rect = countdown_text.get_rect()
+    text_rect.center = (constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    screen.blit(countdown_text, text_rect)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -49,12 +73,24 @@ while True:
         collisions_detection(first_player_racket, second_player_racket, ball)
 
         if ball.top <= -12:
+            count_flag = True
+            score_time = pygame.time.get_ticks()
             first_player_racket.score += 1
         elif ball.bottom >= constants.SCREEN_HEIGHT + 15:
+            count_flag = True
+            score_time = pygame.time.get_ticks()
             second_player_racket.score += 1
 
         first_player_racket.draw_score(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2 + 50)
         second_player_racket.draw_score(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2 - 50)
+
+        if count_flag:
+            current_time = pygame.time.get_ticks()
+            ball.x = constants.SCREEN_WIDTH / 2
+            ball.y = constants.SCREEN_HEIGHT / 2
+            print_countdown(score_time)  # Pass score_time to the function
+            if current_time - score_time >= 2100:
+                count_flag = False  # Reset count_flag when countdown is done
         pygame.display.update()
 
     clock.tick(constants.FPS)
